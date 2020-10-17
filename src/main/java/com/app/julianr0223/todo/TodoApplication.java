07c144792dda8acf1086c96d36932ca83a06776a
Julian Rincon <julian.vera@ceiba.com.co>
2020-10-16 22:32:37 -0500

    creating project

---
+++ src/main/java/com/app/julianr0223/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package com.app.julianr0223.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
